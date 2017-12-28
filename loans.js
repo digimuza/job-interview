154e27f56c5c6e574403d04980f751993121e069
andrius <andrius.mozuraitis@adeoweb.lt>
2017-12-28 20:42:58 +0200

    Loans with dates

--- loans.js
+++ loans.js
@@ -1,202 +1,252 @@
 module.exports = [
     {
-      "loan": "1864.7",
-      "id": 5
+      "registered": "Sunday, July 19, 2015 9:49 AM",
+      "loan": "1868.54",
+      "id": 28
     },
     {
-      "loan": "1313.64",
-      "id": 22
-    },
-    {
-      "loan": "3980.78",
-      "id": 12
-    },
-    {
-      "loan": "3642.66",
-      "id": 0
-    },
-    {
-      "loan": "3427.66",
-      "id": 6
-    },
-    {
-      "loan": "2625.26",
-      "id": 27
-    },
-    {
-      "loan": "1990.28",
-      "id": 18
-    },
-    {
-      "loan": "1997.62",
+      "registered": "Monday, June 29, 2015 11:44 PM",
+      "loan": "1666.94",
       "id": 20
     },
     {
-      "loan": "2509.26",
-      "id": 18
-    },
-    {
-      "loan": "1349.58",
-      "id": 29
-    },
-    {
-      "loan": "2713.05",
-      "id": 11
-    },
-    {
-      "loan": "1214.73",
+      "registered": "Wednesday, February 11, 2015 10:43 PM",
+      "loan": "3982.5",
       "id": 7
     },
     {
-      "loan": "3599.61",
-      "id": 0
-    },
-    {
-      "loan": "3201.78",
-      "id": 4
-    },
-    {
-      "loan": "2652.74",
-      "id": 27
-    },
-    {
-      "loan": "1511.5",
-      "id": 27
-    },
-    {
-      "loan": "2949.56",
-      "id": 24
-    },
-    {
-      "loan": "3710.89",
-      "id": 23
-    },
-    {
-      "loan": "1515.87",
-      "id": 4
-    },
-    {
-      "loan": "3102.76",
-      "id": 3
-    },
-    {
-      "loan": "1316.23",
-      "id": 24
-    },
-    {
-      "loan": "1272.1",
-      "id": 10
-    },
-    {
-      "loan": "3545.17",
-      "id": 12
-    },
-    {
-      "loan": "3333.25",
-      "id": 13
-    },
-    {
-      "loan": "2575.15",
-      "id": 23
-    },
-    {
-      "loan": "1540.8",
-      "id": 7
-    },
-    {
-      "loan": "2561.83",
+      "registered": "Thursday, December 3, 2015 6:02 PM",
+      "loan": "2841.02",
       "id": 2
     },
     {
-      "loan": "1901.02",
-      "id": 28
+      "registered": "Wednesday, May 10, 2017 7:15 PM",
+      "loan": "1635.98",
+      "id": 15
     },
     {
-      "loan": "1467.7",
-      "id": 10
-    },
-    {
-      "loan": "2517.97",
-      "id": 25
-    },
-    {
-      "loan": "1824.21",
-      "id": 1
-    },
-    {
-      "loan": "2112.84",
+      "registered": "Friday, March 25, 2016 2:17 PM",
+      "loan": "3226.99",
       "id": 6
     },
     {
-      "loan": "2075.86",
-      "id": 8
-    },
-    {
-      "loan": "3113.71",
-      "id": 29
-    },
-    {
-      "loan": "2286.09",
-      "id": 20
-    },
-    {
-      "loan": "3124.95",
-      "id": 22
-    },
-    {
-      "loan": "2509.51",
-      "id": 6
-    },
-    {
-      "loan": "2606.61",
-      "id": 8
-    },
-    {
-      "loan": "1893.48",
-      "id": 28
-    },
-    {
-      "loan": "1094.94",
-      "id": 10
-    },
-    {
-      "loan": "3376.41",
-      "id": 11
-    },
-    {
-      "loan": "2366.31",
-      "id": 20
-    },
-    {
-      "loan": "3930.81",
-      "id": 7
-    },
-    {
-      "loan": "3372.86",
-      "id": 14
-    },
-    {
-      "loan": "3378.51",
-      "id": 29
-    },
-    {
-      "loan": "3993.41",
-      "id": 20
-    },
-    {
-      "loan": "2281.74",
-      "id": 22
-    },
-    {
-      "loan": "3545.67",
+      "registered": "Friday, February 13, 2015 3:22 PM",
+      "loan": "1775.49",
       "id": 0
     },
     {
-      "loan": "1727.87",
+      "registered": "Thursday, July 3, 2014 11:49 AM",
+      "loan": "2202.31",
+      "id": 10
+    },
+    {
+      "registered": "Thursday, January 28, 2016 1:06 AM",
+      "loan": "1113.55",
+      "id": 3
+    },
+    {
+      "registered": "Wednesday, September 10, 2014 6:06 PM",
+      "loan": "3418.01",
+      "id": 15
+    },
+    {
+      "registered": "Thursday, March 9, 2017 1:52 PM",
+      "loan": "3001.46",
+      "id": 6
+    },
+    {
+      "registered": "Friday, March 7, 2014 1:08 PM",
+      "loan": 2297,
+      "id": 13
+    },
+    {
+      "registered": "Tuesday, March 7, 2017 6:18 AM",
+      "loan": "1862.22",
+      "id": 16
+    },
+    {
+      "registered": "Thursday, June 5, 2014 8:09 PM",
+      "loan": "1163.23",
+      "id": 10
+    },
+    {
+      "registered": "Wednesday, December 30, 2015 7:05 PM",
+      "loan": "3017.16",
+      "id": 13
+    },
+    {
+      "registered": "Sunday, November 19, 2017 3:03 PM",
+      "loan": "2428.67",
+      "id": 17
+    },
+    {
+      "registered": "Saturday, March 28, 2015 4:56 PM",
+      "loan": "2153.71",
+      "id": 12
+    },
+    {
+      "registered": "Saturday, February 13, 2016 12:51 PM",
+      "loan": "2936.72",
+      "id": 5
+    },
+    {
+      "registered": "Thursday, July 20, 2017 3:13 AM",
+      "loan": "3420.44",
+      "id": 8
+    },
+    {
+      "registered": "Friday, May 1, 2015 1:31 PM",
+      "loan": "3046.88",
+      "id": 0
+    },
+    {
+      "registered": "Sunday, March 23, 2014 6:38 AM",
+      "loan": "3104.9",
+      "id": 19
+    },
+    {
+      "registered": "Sunday, February 21, 2016 8:10 AM",
+      "loan": "3401.84",
+      "id": 22
+    },
+    {
+      "registered": "Friday, January 31, 2014 7:58 PM",
+      "loan": "2663.95",
+      "id": 5
+    },
+    {
+      "registered": "Saturday, August 19, 2017 6:58 AM",
+      "loan": "1511.37",
+      "id": 18
+    },
+    {
+      "registered": "Sunday, July 24, 2016 12:06 PM",
+      "loan": "2084.13",
+      "id": 27
+    },
+    {
+      "registered": "Thursday, December 25, 2014 8:08 PM",
+      "loan": "1348.76",
+      "id": 17
+    },
+    {
+      "registered": "Tuesday, August 18, 2015 10:02 PM",
+      "loan": "2738.9",
+      "id": 0
+    },
+    {
+      "registered": "Monday, September 22, 2014 11:43 PM",
+      "loan": "1847.83",
+      "id": 9
+    },
+    {
+      "registered": "Wednesday, October 25, 2017 1:24 AM",
+      "loan": "2619.25",
+      "id": 12
+    },
+    {
+      "registered": "Wednesday, June 7, 2017 9:54 PM",
+      "loan": "2639.13",
+      "id": 1
+    },
+    {
+      "registered": "Tuesday, November 11, 2014 11:50 PM",
+      "loan": "3876.58",
+      "id": 21
+    },
+    {
+      "registered": "Tuesday, September 6, 2016 2:34 AM",
+      "loan": "2819.99",
       "id": 20
     },
     {
-      "loan": "3538.59",
+      "registered": "Friday, February 14, 2014 4:38 AM",
+      "loan": "2193.07",
+      "id": 4
+    },
+    {
+      "registered": "Monday, September 18, 2017 11:13 AM",
+      "loan": "2381.99",
+      "id": 25
+    },
+    {
+      "registered": "Sunday, March 20, 2016 12:10 PM",
+      "loan": "3266.25",
+      "id": 10
+    },
+    {
+      "registered": "Friday, October 28, 2016 5:35 AM",
+      "loan": "3236.41",
+      "id": 5
+    },
+    {
+      "registered": "Saturday, October 28, 2017 2:47 AM",
+      "loan": "1816.45",
       "id": 14
+    },
+    {
+      "registered": "Friday, August 8, 2014 9:51 AM",
+      "loan": "1028.78",
+      "id": 5
+    },
+    {
+      "registered": "Monday, December 22, 2014 12:46 AM",
+      "loan": "2498.01",
+      "id": 1
+    },
+    {
+      "registered": "Tuesday, February 4, 2014 10:11 PM",
+      "loan": "1581.54",
+      "id": 13
+    },
+    {
+      "registered": "Wednesday, May 31, 2017 5:31 PM",
+      "loan": "2094.85",
+      "id": 24
+    },
+    {
+      "registered": "Tuesday, September 22, 2015 2:56 AM",
+      "loan": "2107.76",
+      "id": 18
+    },
+    {
+      "registered": "Tuesday, September 8, 2015 10:42 AM",
+      "loan": "3730.68",
+      "id": 2
+    },
+    {
+      "registered": "Thursday, March 31, 2016 3:46 PM",
+      "loan": "3709.18",
+      "id": 22
+    },
+    {
+      "registered": "Sunday, December 4, 2016 10:50 AM",
+      "loan": "1273.94",
+      "id": 27
+    },
+    {
+      "registered": "Saturday, June 10, 2017 11:37 AM",
+      "loan": "3797.39",
+      "id": 23
+    },
+    {
+      "registered": "Sunday, September 28, 2014 12:17 AM",
+      "loan": "1178.51",
+      "id": 5
+    },
+    {
+      "registered": "Thursday, May 14, 2015 12:00 PM",
+      "loan": "1246.15",
+      "id": 11
+    },
+    {
+      "registered": "Saturday, February 1, 2014 10:29 AM",
+      "loan": "2505.02",
+      "id": 13
+    },
+    {
+      "registered": "Tuesday, April 4, 2017 2:58 PM",
+      "loan": "3616.14",
+      "id": 3
     }
   ]
