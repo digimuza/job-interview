5ca914dda8fa96283be2f779a04d192ea46ba7fe
andrius <andrius.mozuraitis@adeoweb.lt>
2017-12-28 19:47:29 +0200

    Additional tasks

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ const users = require("./users");
 //Loans
 const loans = require("./loans");
 
-//Assignment 1 - Calculate how much debt does Logan Hudson have?
+//Assignment 1 - Calculate how much debt does Melissa Moore have?
+
+/*
+    Aditional Tasks:
+        Find person who is youngest and have most of debt 
+        How mutch debt was given from Thursday, September 28, 2017 6:35 AM
+
+*/
